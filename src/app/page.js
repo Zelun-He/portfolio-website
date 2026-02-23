4afbd41728afa3245ac7346cbfc4b677518f86f5
Zelun-He <156643164+Zelun-He@users.noreply.github.com>
2026-02-23 15:37:13 -0600

    Add explicit Vercel build configuration

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
+import JourneyTimeline from "./components/JourneyTimeline";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 
@@ -15,6 +16,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection/>
+        <JourneyTimeline/>
         <ProjectsSection/>
         <EmailSection/>
       </div>
